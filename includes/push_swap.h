b0e11bdc999f5347bbd900b44d5a3eb66d8c0c78
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-08-18 00:46:38 +0000

    norm

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,12 +6,12 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 00:30:49 by marvin            #+#    #+#             */
-/*   Updated: 2025/08/18 00:22:04 by marvin           ###   ########.fr       */
+/*   Updated: 2025/08/18 00:46:29 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSHSWAP_H
-# define PUSHSWAP_H
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
 # include <stdlib.h>
 # include "libft.h"
@@ -37,7 +37,6 @@
 # define MSG_EIO "Invalid input."
 # define MSG_EOVERFLOW "Numerical overflow in arguments."
 
-
 # define ORDER_0 0
 # define ORDER_1 1
 # define ORDER_12 12
@@ -49,35 +48,35 @@
 # define ORDER_312 312
 # define ORDER_321 321
 
-#define SORT_ASC 1
-#define SORT_NO 0
-#define SORT_DESC -1
+# define SORT_ASC 1
+# define SORT_NO 0
+# define SORT_DESC -1
 
-#define ORDER_ASC 1
-#define ORDER_DESC -1
+# define ORDER_ASC 1
+# define ORDER_DESC -1
 
-#define PUSH_A2B 1
-#define PUSH_B2A -1
+# define PUSH_A2B 1
+# define PUSH_B2A -1
 
-#define SA 2
-#define SB -2
-#define SS 20
-#define PA 1
-#define PB -1
-#define RA 3
-#define RRA -3
-#define RB 4
-#define RRB -4
-#define RR 5
-#define RRR -5
+# define SA 2
+# define SB -2
+# define SS 20
+# define PA 1
+# define PB -1
+# define RA 3
+# define RRA -3
+# define RB 4
+# define RRB -4
+# define RR 5
+# define RRR -5
 
-typedef	struct	s_psdata
+typedef struct s_psdata
 {
 	int	value;
 	int	rank;
 }	t_psdata;
 
-typedef struct	s_cdlist
+typedef struct s_cdlist
 {
 	struct s_psdata	*content;
 	struct s_cdlist	*next;
@@ -97,8 +96,8 @@ typedef struct s_psstacks
 	int			b_sort_len;
 }	t_psstacks;
 
-int	validate_args(int argc, char **argv);
-int	ft_perror(int err_code);
+int			validate_args(int argc, char **argv);
+int			ft_perror(int err_code);
 
 t_cdlist	*ft_cdlstinit(void);
 t_cdlist	*create_cdlst_node(const int value);
@@ -108,92 +107,72 @@ t_cdlist	*cdlst_find_nil(t_cdlist *lst);
 t_cdlist	*cdlst_find_head(t_cdlist *lst);
 t_cdlist	*cdlst_find_tail(t_cdlist *lst);
 
-int	insert_nil_next(t_cdlist **lst, t_cdlist *new_next);
-int	insert_nil_prev(t_cdlist **lst, t_cdlist *new_prev);
-int	rotate_forward(t_cdlist **lst);
-int	rotate_backward(t_cdlist **lst);
-int	swap_forward(t_cdlist **lst);
-int	swap_backward(t_cdlist **lst);
+int			insert_nil_next(t_cdlist **lst, t_cdlist *new_next);
+int			insert_nil_prev(t_cdlist **lst, t_cdlist *new_prev);
+int			rotate_forward(t_cdlist **lst);
+int			rotate_backward(t_cdlist **lst);
+int			swap_forward(t_cdlist **lst);
+int			swap_backward(t_cdlist **lst);
 
-void	print_cdlst(t_cdlist *lst, int stack);
-void	print_stacks(t_psstacks *stacks);
+void		print_cdlst(t_cdlist *lst, int stack);
+void		print_stacks(t_psstacks *stacks);
 
 t_cdlist	*pop_cdlstnode(t_cdlist	*node);
 void		*delete_cdlst(t_cdlist **lst);
 
-int	push_to_a_stack(t_psstacks **stacks);
-int	push_to_b_stack(t_psstacks **stacks);
+int			push_to_a_stack(t_psstacks **stacks);
+int			push_to_b_stack(t_psstacks **stacks);
 
 t_psstacks	*init_stacks(int argc, char **argv);
 
-int	w_pa(t_psstacks **stacks);
-int	w_pb(t_psstacks **stacks);
-int	w_sa(t_psstacks **stacks);
-int	w_sb(t_psstacks **stacks);
-int	w_ss(t_psstacks **stacks);
-int	w_ra(t_psstacks **stacks);
-int	w_rb(t_psstacks **stacks);
-int	w_rr(t_psstacks **stacks);
-int	w_rra(t_psstacks **stacks);
-int	w_rrb(t_psstacks **stacks);
-int	w_rrr(t_psstacks **stacks);
+int			w_pa(t_psstacks **stacks);
+int			w_pb(t_psstacks **stacks);
+int			w_sa(t_psstacks **stacks);
+int			w_sb(t_psstacks **stacks);
+int			w_ss(t_psstacks **stacks);
+int			w_ra(t_psstacks **stacks);
+int			w_rb(t_psstacks **stacks);
+int			w_rr(t_psstacks **stacks);
+int			w_rra(t_psstacks **stacks);
+int			w_rrb(t_psstacks **stacks);
+int			w_rrr(t_psstacks **stacks);
 
-int		if_stack_a_sorted(t_psstacks *stacks);
-int		if_stack_b_sorted(t_psstacks *stacks);
-int	if_stacks_sorted(t_psstacks *stacks);
+int			if_stack_a_sorted(t_psstacks *stacks);
+int			if_stack_b_sorted(t_psstacks *stacks);
+int			if_stacks_sorted(t_psstacks *stacks);
 
-int	stack_top3_status(t_cdlist *stack);
-int	do_ops(t_psstacks **stacks);
-int	sort_top3_stacks(t_psstacks **stacks, int a_dir, int b_dir);
-
-int	rotate_merge(
-	t_psstacks **stacks,
-	int order,
-	int push_toward
-);
-
-int	rev_rotate_merge(
-	t_psstacks **stacks,
-	int r_count,
-	int order,
-	int push_toward
-);
-int	cdlist_count_sorted(t_cdlist *head);
+int			stack_top3_status(t_cdlist *stack);
+int			do_ops(t_psstacks **stacks);
+int			sort_top3_stacks(t_psstacks **stacks, int a_dir, int b_dir);
+int			cdlist_count_sorted(t_cdlist *head);
 
 t_cdlist	*create_stack_from_arr(int *arr, int len);
-t_psstacks	*create_stacks_from_cdlist(t_cdlist *stack_a, t_cdlist *stack_b, t_cdlist *stack_ops);
-void	free_cdlst_node(t_cdlist *lst);
-int		cmp_cdlst(t_cdlist	*stack_1, t_cdlist *stack_2);
-int	cdlst_len(t_cdlist *lst);
-int	cmp_stacks(t_psstacks *stacks_1, t_psstacks *stacks_2);
-int	w_pan(t_psstacks **stacks, int n);
-int	w_pbn(t_psstacks **stacks, int n);
-void	free_stacks(t_psstacks **stacks);
-int	vsort_top3_stacks(t_psstacks **stack);
-int	msort_top3_stacks(t_psstacks **stack);
-int	asort_top3_stacks(t_psstacks **stack);
-int	dsort_top3_stacks(t_psstacks **stack);
-int	cdlist_count_sorted(t_cdlist *lst);
+t_psstacks	*create_stacks_from_cdlist(
+				t_cdlist *stack_a,
+				t_cdlist *stack_b,
+				t_cdlist *stack_ops
+				);
+void		free_cdlst_node(t_cdlist *lst);
+int			cmp_cdlst(t_cdlist	*stack_1, t_cdlist *stack_2);
+int			cdlst_len(t_cdlist *lst);
+int			cmp_stacks(t_psstacks *stacks_1, t_psstacks *stacks_2);
+int			w_pan(t_psstacks **stacks, int n);
+int			w_pbn(t_psstacks **stacks, int n);
+void		free_stacks(t_psstacks **stacks);
+int			vsort_top3_stacks(t_psstacks **stack);
+int			msort_top3_stacks(t_psstacks **stack);
+int			asort_top3_stacks(t_psstacks **stack);
+int			dsort_top3_stacks(t_psstacks **stack);
+int			cdlist_count_sorted(t_cdlist *lst);
 t_psstacks	*init_stacks_from_array(int *arr, int len);
 
-int	top3_sort_test(void);
-int	top2_sort_test(void);
-int	test_vsort_top3(t_psstacks *sorted, int *arr, int len, int print);
-int	test_msort_top3(t_psstacks *sorted, int *arr, int len, int print);
-int	test_asort_top3(t_psstacks *sorted, int *arr, int len, int print);
-int	test_dsort_top3(t_psstacks *sorted, int *arr, int len, int print);
-int	test_vsort_top2(t_psstacks *sorted, int *arr, int len, int print);
-int	test_msort_top2(t_psstacks *sorted, int *arr, int len, int print);
-int	test_asort_top2(t_psstacks *sorted, int *arr, int len, int print);
-int	test_dsort_top2(t_psstacks *sorted, int *arr, int len, int print);
+int			presort_stacks(t_psstacks **stacks);
+int			push_all_to_stack_a(t_psstacks **stacks);
+int			push_all_to_stack_b(t_psstacks **stacks);
+int			set_rank_for_sorted(t_psstacks **stacks);
 
-int	presort_stacks(t_psstacks **stacks);
-int	push_all_to_stack_a(t_psstacks **stacks);
-int	push_all_to_stack_b(t_psstacks **stacks);
-int	set_rank_for_sorted(t_psstacks **stacks);
-
-void	reverse_sorted(t_psstacks **stacks);
-int	print_sort_ops(t_psstacks **stacks);
-int	split_argv(int *argc, char ***argv);
-int	naive_radix_sort(t_psstacks **stacks, int n);
+void		reverse_sorted(t_psstacks **stacks);
+int			print_sort_ops(t_psstacks **stacks);
+int			split_argv(int *argc, char ***argv);
+int			naive_radix_sort(t_psstacks **stacks, int n);
 #endif
